ded3f995812ae87473283dce4082ca8ec25f494d
Jess Pesale <jess_pesale@aol.com>
2021-01-11 23:00:06 -0500

    start of scraping

--- lib/make_a_cocktail/scraper.rb
+++ lib/make_a_cocktail/scraper.rb
@@ -4,14 +4,34 @@ class MakeACocktail::Scraper
         url = "https://www.liquor.com/classic-cocktails-4779424"
 
         website = Nokogiri::HTML(open(url))
-        binding.pry
+        
+        array_of_drinks = website.css("div.loc div.card-list__item")
+        
+        array_of_drinks.each do |drink|
+            drink_name = drink.css("span.card__underline").text
+            binding.pry
 
+            
+        end
 
     end
 
-    def scrape_recipe
+    def self.scrape_recipe(drink)
+        drink = {}
+        drink_page = Nokogiri::HTML(open())
 
     end
 
 
 end
+
+
+
+
+
+# puts section.count	= 38 (which is what we want)^
+
+# section.each do |drink|
+#   puts drink.css("span.card__underline")
+# end
+#  = all the drink names
