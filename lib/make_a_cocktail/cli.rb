ed97ff0a4f5ba808a2763589d7c900645045e478
Jess Pesale <jess_pesale@aol.com>
2021-01-14 18:57:46 -0500

    begining of the cli

--- lib/make_a_cocktail/cli.rb
+++ lib/make_a_cocktail/cli.rb
@@ -1,20 +1,37 @@
 class MakeACocktail::CLI
 
     def start
-        puts "Welcome, let's make a drink!" 
-        puts "Here is the list of drinks to choose from:"
-        #scrape all the cocktails - call the scraper class
-        # list all the cocktails
-        # MakeACocktail::Scraper
-        #list_drinks
+        puts "\nWelcome, let's make a drink!" 
+        get_drinks
+        list_drinks
+        get_users_drink
         #  MakeACocktail::Scraper.scrape_drinks
+        # binding.pry
+    end
+
+    def get_drinks
+        # to be scraped
+        @drinks = [ "LIT", "Baybreeze", "Vodka Soda"]
     end
 
     def list_drinks
-
+        puts "\nHere is the list of drinks to choose from:"
+        @drinks.each_with_index do |drink, index| 
+        puts "#{index + 1}. #{drink}" 
+        end
     end
 
+    def get_users_drink
+        chosen_drink = gets.strip
+        binding.pry
+        # if valid_input(chosen_drink.to_i)
 
+        # end
+    end
+
+    def valid_input(input)
+        input <= @drinks.length && input > 0
+    end
 
 
 end
