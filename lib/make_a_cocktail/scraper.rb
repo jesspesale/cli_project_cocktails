4f70c735dc804235a008db40c8fc9fd88b99d7e9
Jess Pesale <jess_pesale@aol.com>
2021-01-19 15:00:05 -0500

    fixing scraping the ingredients

--- lib/make_a_cocktail/scraper.rb
+++ lib/make_a_cocktail/scraper.rb
@@ -2,6 +2,7 @@ class MakeACocktail::Scraper
 
     def self.scrape_drinks
         @url = "https://www.liquor.com/classic-cocktails-4779424"
+        # https://www.thespruceeats.com/old-fashioned-cocktail-recipe-and-history-759328 - has js in the ingredients
 
         website = Nokogiri::HTML(open(@url))
         drinks = []
@@ -26,19 +27,29 @@ class MakeACocktail::Scraper
         drink_recipe = {}
    
         doc = Nokogiri::HTML(open(drink_url))
-        ingredients_section = doc.css("li.structured-ingredients__list-item").text
-        ingredients_array = ingredients_section.split("\n")
-        no_empty_strings = ingredients_array.reject { |i| i.empty?}
-        @first_ingredient = no_empty_strings[0]
-        @second_ingredient =  no_empty_strings[1]
-        @third_ingredient = no_empty_strings[2]
-        @fourth_ingredient = no_empty_strings[3]
-        @
+
+        if drink_url == "https://www.liquor.com/parisian-blonde-cocktail-recipe-5091868"
+            ingredients_section = doc.css("li.structured-ingredients__list-item").text
+            ingredients_array = ingredients_section.split("\n")
+            no_empty_strings = ingredients_array.reject { |i| i.empty?}
+            # binding.pry
+            no_empty_strings
+        else 
+            ingredients_arr = doc.css("li.simple-list__item").text.split("\n")
+            ingredients = ingredients_arr.reject{ |i| i.empty?}
+            ingredients
+
+            # binding.pry
+        end
+            
+        end
         
-        binding.pry 
-        
-    end
-    
+        # @first_ingredient = no_empty_strings[0]
+        # @second_ingredient =  no_empty_strings[1]
+        # @third_ingredient = no_empty_strings[2]
+        # @fourth_ingredient = no_empty_strings[3]
+
+        # binding.pry 
     # def self.scrape_url
     #     doc = Nokogiri::HTML(open(@url))
 
